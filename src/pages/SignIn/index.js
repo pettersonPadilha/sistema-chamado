6f2ca1d3883081c2df85356baaf661f14fd917af
petter Padilha <petterson_sorriso@hotmail.com>
2022-11-13 21:55:27 -0500

    Criando a estrutura da pagina de login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,25 @@
+import { useState } from 'react'
+import {Link} from 'react-router-dom'
+import './signin.css';
+import logo from '../../assets/perfil01.png'
 export default function SignIn() {
-    return(
-        <div>
-            <h1>Bem vindo tela Sign In</h1>
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  return(
+    <div className="container-center">
+        <div className="login">
+            <div className="logo-area">
+                <img src={logo} alt="logo"/>
+            </div>
+            <form>
+                <h1>Entrar</h1>
+                <input type="text" placeholder="Digite seu email"/>
+                <input type="password" placeholder="Digite sua senha"/>
+                <button type="submit">Acessar</button>
+            </form>
+            <Link to="/register">Criar uma conta</Link>
         </div>
-    );
+    </div>
+  )
 }
